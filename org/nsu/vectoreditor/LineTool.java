35283bb2365118b5a48a0054582f7d0359298f3d
Alexander <esil@esil-notebook.(none)>
2011-10-28 17:30:30 +0700

    added State to LineTool

--- org/nsu/vectoreditor/LineTool.java
+++ org/nsu/vectoreditor/LineTool.java
@@ -3,91 +3,134 @@
 package org.nsu.vectoreditor;
 
 
-public class LineTool extends Tool {
+abstract class LineToolState {
+    public abstract void draw(java.awt.Graphics canvas);
+    public abstract LineToolState onMouseDown(int x, int y);
+    public abstract LineToolState onMouseUp(int x, int y);
+    public abstract LineToolState onMouseMove(int x, int y);
+};
 
-    private enum State {
-        START,
-        FIRST_PRESSED,
-        FREE_MOVE
-    }
 
-    LineTool(ShapeList list) {
+class LineToolStateStart extends LineToolState {
+    public LineToolStateStart(ShapeList list) {
         shapes = list;
-        state = State.START;
     }
 
-    public void draw(java.awt.Graphics canvas) {
+    public void draw(java.awt.Graphics canvas) {}
 
-        if(state == State.START) {
-            return;
-        }
-
-        (new Line(startX, startY, endX, endY)).draw(canvas);
+    public LineToolState onMouseDown(int x, int y) {
+        return new LineToolStatePressed(shapes, x, y);
     }
 
-    public void reset() {
-        state = State.START;
-    }
+    public LineToolState onMouseUp(int x, int y) { return this; }
+    public LineToolState onMouseMove(int x, int y) { return this; }
 
-    public void onMouseDown(int x, int y) {
-        switch(state) {
-        case START:
-            startX = x;
-            startY = y;
-            endX = x;
-            endY = y;
-            state = State.FIRST_PRESSED;
-            break;
+    private ShapeList shapes;
+};
 
-        case FIRST_PRESSED:
-            break;
-
-        case FREE_MOVE:
-            break;
-        }
-    }
-
-    public void onMouseUp(int x, int y) {
-        switch(state) {
-        case START:
-            break;
-
-        case FIRST_PRESSED:
-            if(startX == x && startY == y) {
-                state = State.FREE_MOVE;
-            } else {
-                shapes.add(new Line(startX, startY, x, y));
-                state = State.START;
-            }
-            break;
-
-        case FREE_MOVE:
-            shapes.add(new Line(startX, startY, x, y));
-            state = State.START;
-            break;
-        }
-    }
-
-    public void onMouseMove(int x, int y) {
-        switch(state) {
-        case START:
-            break;
-
-        case FIRST_PRESSED:
-        case FREE_MOVE:
-            endX = x;
-            endY = y;
-            break;
-        }
+class LineToolStatePressed extends LineToolState {
+    public LineToolStatePressed(ShapeList list, int x, int y) {
+        shapes = list;
+        startX = x;
+        startY = y;
         endX = x;
         endY = y;
     }
 
-    ShapeList shapes;
-    State state;
+    public void draw(java.awt.Graphics canvas) {
+        (new Line(startX, startY, endX, endY)).draw(canvas);
+    }
+
+    public LineToolState onMouseDown(int x, int y) {
+        return this;
+    }
+
+    public LineToolState onMouseUp(int x, int y) {
+        if(x == startX && y == startY) {
+            return new LineToolStateFreeMove(shapes, x, y);
+        } else {
+            shapes.add(new Line(startX, startY, x, y));
+            return new LineToolStateStart(shapes);
+        }
+    }
+
+    public LineToolState onMouseMove(int x, int y) {
+        endX = x;
+        endY = y;
+        return this;
+    }
+
+    private ShapeList shapes;
     int startX;
     int startY;
     int endX;
     int endY;
+};
+
+class LineToolStateFreeMove extends LineToolState {
+    public LineToolStateFreeMove(ShapeList list, int x, int y) {
+        shapes = list;
+        startX = x;
+        startY = y;
+        endX = x;
+        endY = y;
+    }
+
+    public void draw(java.awt.Graphics canvas) {
+        (new Line(startX, startY, endX, endY)).draw(canvas);
+    }
+
+    public LineToolState onMouseDown(int x, int y) {
+        return this;
+    }
+
+    public LineToolState onMouseUp(int x, int y) {
+        shapes.add(new Line(startX, startY, x, y));
+        return new LineToolStateStart(shapes);
+    }
+
+    public LineToolState onMouseMove(int x, int y) {
+        endX = x;
+        endY = y;
+        return this;
+    }
+
+    private ShapeList shapes;
+    int startX;
+    int startY;
+    int endX;
+    int endY;
+};
+
+
+public class LineTool extends Tool {
+
+    LineTool(ShapeList list) {
+        shapes = list;
+        state = new LineToolStateStart(shapes);
+    }
+
+    public void draw(java.awt.Graphics canvas) {
+        state.draw(canvas);
+    }
+
+    public void reset() {
+        state = new LineToolStateStart(shapes);
+    }
+
+    public void onMouseDown(int x, int y) {
+        state = state.onMouseDown(x, y);
+    }
+
+    public void onMouseUp(int x, int y) {
+        state = state.onMouseUp(x, y);
+    }
+
+    public void onMouseMove(int x, int y) {
+        state = state.onMouseMove(x, y);
+    }
+
+    ShapeList shapes;
+    LineToolState state;
 }
 
